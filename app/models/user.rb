ba7d9f7ca57bf98c41340ddebde9a9c18942edfc
hiro-mu <71165638+hiro-mu@users.noreply.github.com>
2021-01-16 22:08:28 +0900

    Revert "配布ファイルの置き換え"
    This reverts commit aab1e3bd786449b3eb9b569b8f835143ee7d6644.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :kanji_name, presence: true
-  validates :kanji_lastname, presence: true
-  validates :kana_name, presence: true
-  validates :kana_lastname, presence: true
-  validates :birthday, presence: true
-
 end
