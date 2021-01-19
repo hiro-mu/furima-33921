3b01a12e5b3ed038b721d776c03dd869e278fb44
hiro-mu <71165638+hiro-mu@users.noreply.github.com>
2021-01-19 22:16:28 +0900

    出品商品保存の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,18 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :information, :category_id, :condition_id, :shipping_fee_type_id, :prefecture_id, :days_to_ship_id, :price)
+  end
 end
