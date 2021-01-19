63f625d6f36a2589dbe67a626f0b15b2f233a74d
hiro-mu <71165638+hiro-mu@users.noreply.github.com>
2021-01-19 11:12:40 +0900

    バリデーションの統合

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,10 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
   validates :nickname, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
     validates :kanji_name
     validates :kanji_lastname
