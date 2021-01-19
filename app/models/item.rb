de05eb522147478bc1b5a852424ec36b6f946ded
hiro-mu <71165638+hiro-mu@users.noreply.github.com>
2021-01-19 21:26:45 +0900

    ActiveHash機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :days_to_ship
+  belongs_to :prefecture
+  belongs_to :shipping_fee_type
+
+  with_options presence: true do
+    validates :name
+    validates :information
+    validates :price
+  end
+
+  with_options numericality:{ other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :days_to_ship_id
+    validates :prefecture_id
+    validates :shipping_fee_type_id
+  end
 end
