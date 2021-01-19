5512466235499855fc4cc797565cb796f0ac59c9
hiro-mu <71165638+hiro-mu@users.noreply.github.com>
2021-01-19 22:04:11 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to :days_to_ship
   belongs_to :prefecture
   belongs_to :shipping_fee_type
+  has_one_attached :image
 
   with_options presence: true do
     validates :name
