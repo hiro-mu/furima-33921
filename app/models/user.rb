bfa0f6581bf042e4dc1b9ee31a073cbeef7e0f73
hiro-mu <71165638+hiro-mu@users.noreply.github.com>
2021-01-20 18:21:05 +0900

    不要な記述の削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-  validates :nickname, presence: true
   with_options presence: true do
     validates :nickname
     validates :birthday
