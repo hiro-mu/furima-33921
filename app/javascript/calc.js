7ace9ed150d1b27fc0438573ed430fa3cc92eb28
hiro-mu <71165638+hiro-mu@users.noreply.github.com>
2021-01-20 14:29:34 +0900

    販売手数料、販売利益表示の実装

---
+++ app/javascript/calc.js
@@ -0,0 +1,12 @@
+function calc (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxPrice = document.getElementById("add-tax-price")
+    addTaxPrice.innerHTML = Math.floor( inputValue * 0.1 )
+    const profitPrice = document.getElementById("profit")
+    profitPrice.innerHTML = Math.floor( inputValue - inputValue * 0.1 )
+  })
+}
+
+window.addEventListener('load', calc);
