283d1456c1541a722af4d81c053ffbd5fae2d05f
hiro-mu <71165638+hiro-mu@users.noreply.github.com>
2021-01-23 11:52:36 +0900

    購入機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,24 @@
 class PurchasesController < ApplicationController
 
-def index
-  @item = Item.find(params[:item_id])
-  @purchase_address = PurchaseAddress.new
-end
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
+  end
 
-def create
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(address_params)
+    if @purchase_address.valid?
+      binding.pry
+      @purchase_address.save
+      redirect_to item_path(@item)
+    else
+      render action: :index
+    end
+  end
 
-end
-
+  private
+  def address_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :telephone_number, :purchase_id).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,7 +1,7 @@
 class PurchaseAddress
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :telephone_number
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :telephone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly" }
@@ -12,7 +12,7 @@ class PurchaseAddress
   validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
 
   def save
-    purchase = Purchase.create
-    Adress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, house_number: house_number, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, house_number: house_number, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end
