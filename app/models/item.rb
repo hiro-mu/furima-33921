72531671b088c4c4be939c187bea4b5069ceef86
hiro-mu <71165638+hiro-mu@users.noreply.github.com>
2021-02-01 15:54:28 +0900

    商品検索機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,4 +27,12 @@ class Item < ApplicationRecord
   validates :price, numericality: { only_integer: true, message: 'Half-width number' }
   validates :price,
             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  
+  def self.search(search)
+    if search != ""
+      Item.where('name LIKE(?)', "%#{search}%")
+    else
+      Item.all
+    end
+  end
 end
